dced61feaa91d766e7ac80b377db2b85a6462450
Gabriel Gomes <gabrielnvgomes@gmail.com>
2019-11-08 16:15:16 -0200

    Add: TheHeader component completed

--- src/components/common/TheHeader.vue
+++ src/components/common/TheHeader.vue
@@ -1,9 +1,14 @@
 <template lang="pug">
-  div
-    h2 Header
-    hr
+  #the-header
 </template>
 
 <script>
 export default {};
 </script>
+
+<style scoped lang="scss">
+  #the-header {
+    height: 80px;
+    background-color: var(--color-primary);
+  }
+</style>
