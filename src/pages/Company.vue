e4069b8ba54b1ffcfcfaf1f65ed4ad4720a23ba6
Gabriel Gomes <gabriel_gom@hotmail.com>
2019-11-17 01:56:42 -0300

    Add: Company component style

--- src/pages/Company.vue
+++ src/pages/Company.vue
@@ -1,9 +1,26 @@
 <template lang="pug">
   #company
-    h1 Company
-    router-view
+    h1 company
+    .company-main-container
+      router-view
 </template>
 
 <script>
 export default {};
 </script>
+
+<style scoped lang="scss">
+  #company {
+    display: flex;
+    flex-direction: column;
+
+    @media screen and (max-width: $breakpoint-lg) {
+      align-content: center;
+      justify-content: space-between;
+    }
+
+    .company-main-container {
+      max-width: 720px;
+    }
+  }
+</style>
