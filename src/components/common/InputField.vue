07783465804779d24b0e0a00a6c8a4470f659d88
Gabriel Gomes <gabrielnvgomes@gmail.com>
2019-11-24 14:16:35 -0200

    Add: InputField component layout only

---
+++ src/components/common/InputField.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+  label
+    | {{ label }}
+    input(type="text", :id="id", :name="id", :placeholder="`e.g. ${placeholder}`")
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String
+    },
+    placeholder: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+  label {
+    margin-top: 30px;
+    max-width: 335px;
+    display: block;
+    font-size: var(--font-sm);
+    color: #B1B1B1;
+    text-transform: uppercase;
+
+    input {
+      padding: 10px 15px;
+      width: 100%;
+      height: 40px;
+      display: block;
+      border: 1px solid var(--color-border);
+      border-radius: 5px;
+      font-size: var(--font-md);
+      color: var(--color-text);
+
+      &::placeholder {
+        color: #CED2E0;
+      }
+    }
+  }
+</style>
--- src/pages/company/CompanyData.vue
+++ src/pages/company/CompanyData.vue
@@ -1,8 +1,46 @@
 <template lang="pug">
   .company-data
-    h2 Company Data
+    p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+    form
+      InputField(
+        id="companyName",
+        label="Company Name",
+        placeholder="Your Company Name"
+      )
+      InputField(
+        id="companySpend",
+        label="Company Spend",
+        placeholder="$150,000"
+      )
+      InputField(
+        id="companySpendAbility",
+        label="Company Spend Ability",
+        placeholder="$150,000 - $330,000"
+      )
 </template>
 
 <script>
-export default {};
+import InputField from 'Components/common/InputField.vue';
+
+export default {
+  components: {
+    InputField
+  },
+};
 </script>
+
+<style scoped lang="scss">
+  .company-data {
+    padding: 20px;
+    background-color: var(--color-background);
+    border: 1px solid var(--color-border);
+    border-radius: 5px;
+    font-size: var(--font-md);
+    color: var(--color-text);
+    line-height: var(--line-height-md);
+
+    form {
+      
+    }
+  }
+</style>
